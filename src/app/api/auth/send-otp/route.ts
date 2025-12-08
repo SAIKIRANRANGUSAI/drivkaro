21ef60071cc073e1f5f4babb3a9f9a44d1b37590
SAIKIRANRANGUSAI <saikiranrangu6@gmail.com>
2025-12-08 10:59:25 +0530

    added

--- src/app/api/auth/send-otp/route.ts
+++ src/app/api/auth/send-otp/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/db";
 import Otp from "@/models/Otp";
 import crypto from "crypto";
+// 
 
 export async function POST(req: Request) {
   await connectDB();
